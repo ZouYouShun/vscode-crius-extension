6b5aaefb791b1eb43c4dfc3e60dab55bdc32f80d
sarah chen <sarah.chen@ringcentral.com>
2021-01-25 22:22:29 +0800

    feat: add withProgress when creating case

--- src/utils/tees/teesCreateCase.ts
+++ src/utils/tees/teesCreateCase.ts
@@ -1,4 +1,4 @@
-import { execSync } from 'child_process';
+import { exec } from 'child_process';
 import * as fs from 'fs';
 import * as vscode from 'vscode';
 import { OutputChannel } from '..';
@@ -14,28 +14,47 @@ export async function teesCreateCase(caseId: string) {
     }
     if (caseId) {
         try {
-            caseType = await vscode.window.showQuickPick(
-                ['E2E', 'UT/IT'], { canPickMany: false }
-            );
+            caseType = await vscode.window.showQuickPick(['E2E', 'UT/IT'], {
+                canPickMany: false,
+            });
         } catch (error) {
             vscode.window.showErrorMessage(error);
         }
         if (caseType === 'UT/IT' && projectList) {
             try {
-                projectPath = await vscode.window.showQuickPick(
-                    projectList, { canPickMany: false }
-                );
+                projectPath = await vscode.window.showQuickPick(projectList, {
+                    canPickMany: false,
+                });
             } catch (error) {
                 vscode.window.showErrorMessage(error);
             }
         }
     }
 
-    const execCommand = caseType === 'E2E' ? `yarn run tees create ${caseId}` : `CASE_TYPE=IT PROJECT_PATH=${projectPath} yarn run tees create ${caseId}`;
-    vscode.window.showInformationMessage(`Creating ${caseType} case RCI-${caseId}......`);
+    const execCommand =
+        caseType === 'E2E'
+            ? `yarn run tees create ${caseId}`
+            : `CASE_TYPE=IT PROJECT_PATH=${projectPath} yarn run tees create ${caseId}`;
 
-    const result = execSync(`cd ${rootPath}/packages/ringcentral-e2e-test/ && ${execCommand}`);
-    console.log(result.toString());
-    
-    OutputChannel.appendLine(result.toString());
+    vscode.window.withProgress(
+        {
+            cancellable: false,
+            location: vscode.ProgressLocation.Notification,
+            title: `Creating ${caseType} case RCI-${caseId} ......`,
+        },
+        async () => {
+            const result = await new Promise<string>((resolve) => {
+                exec(
+                    `cd ${rootPath}/packages/ringcentral-e2e-test/ && ${execCommand}`,
+                    (error, stdout) => {
+                        if (error) {
+                            console.error(`exec error: ${error}`);
+                            return;
+                        }
+                        resolve(stdout);
+                    },
+                );
+            });
+            OutputChannel.appendLine(result);
+        });
 }
