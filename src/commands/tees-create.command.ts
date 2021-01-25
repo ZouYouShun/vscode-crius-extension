7ed5f12188ec0f4a292046501eca2629622ee712
sarah chen <sarah.chen@ringcentral.com>
2021-01-25 22:42:47 +0800

    refactor: remove meaningless showError

--- src/commands/tees-create.command.ts
+++ src/commands/tees-create.command.ts
@@ -4,11 +4,6 @@ import { extensionNamespace, teesCreateCase } from '../utils';
 export const teesCreateCommand = vscode.commands.registerCommand(
   `${extensionNamespace}.teesCreate`,
   async () => {
-    const caseId = await vscode.window.showInputBox({
-      placeHolder: 'Please enter case ID or IDs(eg. 1000 or 1000, 1001)',
-    })
-    if (caseId) {
-      await teesCreateCase(caseId);
-    }
+    await teesCreateCase();
   },
 );
--- src/utils/tees/teesCreateCase.ts
+++ src/utils/tees/teesCreateCase.ts
@@ -3,58 +3,55 @@ import * as fs from 'fs';
 import * as vscode from 'vscode';
 import { OutputChannel } from '..';
 
-export async function teesCreateCase(caseId: string) {
-    let caseType, projectPath;
-    let projectList;
-    const { rootPath } = vscode.workspace;
-    if (rootPath) {
-        fs.readdir(`${rootPath}/packages/`, (err, files) => {
-            projectList = files;
-        });
+export async function teesCreateCase() {
+  const caseId = await vscode.window.showInputBox({
+    placeHolder: 'Please enter case ID or IDs(eg. 1000 or 1000, 1001)',
+  });
+
+  let caseType, projectPath;
+  let projectList;
+  const { rootPath } = vscode.workspace;
+  if (rootPath) {
+    fs.readdir(`${rootPath}/packages/`, (err, files) => {
+      projectList = files;
+    });
+  }
+  if (caseId) {
+    caseType = await vscode.window.showQuickPick(['E2E', 'UT/IT'], {
+      canPickMany: false,
+    });
+    if (caseType === 'UT/IT' && projectList) {
+      projectPath = await vscode.window.showQuickPick(projectList, {
+        canPickMany: false,
+      });
     }
-    if (caseId) {
-        try {
-            caseType = await vscode.window.showQuickPick(['E2E', 'UT/IT'], {
-                canPickMany: false,
-            });
-        } catch (error) {
-            vscode.window.showErrorMessage(error);
-        }
-        if (caseType === 'UT/IT' && projectList) {
-            try {
-                projectPath = await vscode.window.showQuickPick(projectList, {
-                    canPickMany: false,
-                });
-            } catch (error) {
-                vscode.window.showErrorMessage(error);
+  }
+
+  const execCommand =
+    caseType === 'E2E'
+      ? `yarn run tees create ${caseId}`
+      : `CASE_TYPE=IT PROJECT_PATH=${projectPath} yarn run tees create ${caseId}`;
+
+  vscode.window.withProgress(
+    {
+      cancellable: false,
+      location: vscode.ProgressLocation.Notification,
+      title: `Creating ${caseType} case RCI-${caseId} ......`,
+    },
+    async () => {
+      const result = await new Promise<string>((resolve) => {
+        exec(
+          `cd ${rootPath}/packages/ringcentral-e2e-test/ && ${execCommand}`,
+          (error, stdout) => {
+            if (error) {
+              console.error(`exec error: ${error}`);
+              return;
             }
-        }
-    }
-
-    const execCommand =
-        caseType === 'E2E'
-            ? `yarn run tees create ${caseId}`
-            : `CASE_TYPE=IT PROJECT_PATH=${projectPath} yarn run tees create ${caseId}`;
-
-    vscode.window.withProgress(
-        {
-            cancellable: false,
-            location: vscode.ProgressLocation.Notification,
-            title: `Creating ${caseType} case RCI-${caseId} ......`,
-        },
-        async () => {
-            const result = await new Promise<string>((resolve) => {
-                exec(
-                    `cd ${rootPath}/packages/ringcentral-e2e-test/ && ${execCommand}`,
-                    (error, stdout) => {
-                        if (error) {
-                            console.error(`exec error: ${error}`);
-                            return;
-                        }
-                        resolve(stdout);
-                    },
-                );
-            });
-            OutputChannel.appendLine(result);
-        });
+            resolve(stdout);
+          },
+        );
+      });
+      OutputChannel.appendLine(result);
+    },
+  );
 }
